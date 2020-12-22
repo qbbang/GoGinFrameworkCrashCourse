cb525678e4509ad98aa585040c112d3143fadf6f
qbbang <qbbang@gmail.com>
2020-12-22 12:23:39 +0900

    의존성 주입을 위한 리포지토리 경로 생성

---
+++ 01|GettingStarted/go.mod
@@ -0,0 +1,8 @@
+module gitlab.com/pragmaticareviews/golang-gin-poc
+
+go 1.15
+
+require (
+	github.com/gin-gonic/gin v1.6.3 // indirect
+	github.com/go-delve/delve v1.5.1 // indirect
+)
---
+++ 01|GettingStarted/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/go-delve/delve/service"
+)
+
+var (
+	videoService service.VideoService       = service.New()
+	videoService controller.VideoController = controller.New(videoService)
+)
+
+func main() {
+	// 서버 초기화
+	server := gin.Default()
+
+	// 첫번째 엔드 포인트 - Context으로 미드뤠어간에 변수를 전달하고, 흐름을 관리, JSON
+	/*
+		server.GET("/test", func(ctx *gin.Context) {
+			ctx.JSON(200, gin.H{
+				"message": "OK!!",
+			})
+		})
+	*/
+
+	server.GET("/videos", func(ctx *gin.Context) {
+		ctx.JSON(200, videoController.FindAll())
+	})
+
+	server.POST("/videos", func(ctx *gin.Context) {
+		ctx.JSON(200, videoController.Save(ctx))
+
+	})
+
+	server.Run(":8080")
+
+}
